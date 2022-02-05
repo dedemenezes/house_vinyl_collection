bffa7f69fcdc9044c878ece01590d0e0540505ef
Andre Menezes <djdedemenezes@gmail.com>
2022-02-05 20:42:32 -0300

    album done

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,7 @@
 class Album < ApplicationRecord
   belongs_to :artist
+
+  validates :name, :year, presence: true
+  validates :year, numericality: { only_integer: true, less_than: Time.now.year }
+  validates :name, length: { minimum: 2 }
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:year) }
+    it { should validate_presence_of(:year) }
+    it { should validate_numericality_of(:year).is_less_than(Time.now.year) }
+    it { should validate_length_of(:name).is_at_least(2).on(:create) }
+  end
+
+  describe 'Associations' do
+    it { should belong_to(:artist) }
+  end
 end
